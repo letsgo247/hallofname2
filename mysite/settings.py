268a3926a8bdf16214356cc37214bd263ba0b543
letsgo247 <letsgo247@hanmail.net>
2017-11-17 11:53:02 +0900

    ready for pythonanywhere 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+f!dbh2nczo-v#nqy*(rf*zje&)ax-g$kbbbwkel6o4p2h2*2@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com', '192.168.0.7']
 
 # Application definition
 
